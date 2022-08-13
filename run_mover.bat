8c2db583b19124e600c03952cd7f3546f34593a7
Eeeeelias <Elias.Albrecht@campus.lmu.de>
2022-08-13 11:38:09 +0200

    added example batch file for windows

---
+++ run_mover.bat
@@ -0,0 +1,5 @@
+Rem batch script that is executable by clicking on it. --sv are for special Episodes with non-clear Season markings.
+Rem See media_mover -h for more info.
+@echo off
+
+python3 "C:\<path>\<to>\<script>\media_mover.py" --op "P:\Downloaded Shows" --dp "P:\Plex Shows" --sv Shugi;2 Maou;2
