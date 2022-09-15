57f5f65a272ae2f0708ace2763bcc3a15b2941bd
Eeeeelias <Elias.Albrecht@campus.lmu.de>
2022-09-15 22:24:43 +0200

    possibility to write to csv

--- fetch_show_infos.py
+++ fetch_show_infos.py
@@ -1,3 +1,4 @@
+import csv
 import glob
 import re
 from sys import platform
@@ -12,11 +13,13 @@ else:
 
 
 def get_infos(plex_path: AnyStr, shows=True):
+    plex_path = plex_path.rstrip(sep)
     drop = 'TV Shows'
     if not shows:
         drop = 'Movies'
 
     show_infos = []
+    show_nr = 0
     show = ""
     seasons = 0
     episodes = 0
@@ -25,13 +28,21 @@ def get_infos(plex_path: AnyStr, shows=True):
         # parts gives [show, season, episode]
         parts = list(dropwhile(lambda x: x != drop, media.split(sep)))[1:]
         if show == "":
+            print(f"Show: {parts[0]}")
             show = parts[0]
+            show_nr = 1
         if show == parts[0]:
-            seasons = int(re.search(r"\d+", parts[1]).group())
+            try:
+                seasons = int(re.search(r"\d+", parts[1]).group())
+            except AttributeError:
+                pass
             episodes = episodes + 1
             size += os.path.getsize(media)
+
             continue
-        show_infos.append({"show_name": show, "seasons": seasons, "episodes": episodes, "show_size": size})
+        show_infos.append({"show_nr": show_nr, "show_name": show, "seasons": seasons, "episodes": episodes, "show_size": size})
+        print(f"Show: {parts[0]}")
+        show_nr += 1
         show = parts[0]
         seasons = 1
         episodes = 1
@@ -49,6 +60,17 @@ def print_infos(show_infos: List):
     return
 
 
+def write_to_csv(show_infos: List):
+    with open('show_infos.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['Number', 'Name', 'Seasons', 'Episodes', 'Show size'])
+        for show in show_infos:
+            csvwriter.writerow(show.values())
+
+
 if __name__ == '__main__':
-    info = get_infos("P:\\script_testing\\TV Shows", shows=True)
-    print_infos(info)
+    # P:\\Plex Shows\\TV Shows
+    # P:\\script_testing\\TV Shows
+    info = get_infos("P:\\Plex Shows\\TV Shows", shows=True)
+    #print_infos(info)
+    write_to_csv(info)
