86e6823cf521ac84f00f21e0b295280bd4d02f3f
Eeeeelias <Elias.Albrecht@campus.lmu.de>
2022-09-18 15:36:26 +0200

    first interaction with db

--- playground.py
+++ playground.py
@@ -1,13 +1,16 @@
 import os
-
-import fetch_infos
 import manage_db
+import sys
+from media_mover import data_path
 
-db_path = os.getenv('LOCALAPPDATA') + "\\pmm\\media_database.db"
+db_path = data_path
 # db_path = "test_db.db"
 
+if input("Would you like to look for a [s]how or a [m]ovie? ") == "s":
+    show = input("Put in a name for your search: ")
+    res = manage_db.get_shows(search=show, db_path=db_path)
+else:
+    movie = input("Put in a name for your search: ")
+    res = manage_db.get_shows(search=movie, db_path=db_path)
 
-res = manage_db.get_shows(search="Yome", db_path=db_path)
 manage_db.print_shows(res)
-
-res = manage_db.get_newest("shows", 1663504832.285441, db_path)
